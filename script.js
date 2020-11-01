df2cc2ae8e8d1efa3c5d4067756c191505d9cb17
fortuna8.2 <fortuna8228@gmail.com>
2020-11-01 10:24:11 +0200

    добавил градиенты в песок

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const hourglass3 = document.querySelector('.hourglass-3');
 const hourglass4 = document.querySelector('.hourglass-4');
+const hourglassMask = document.querySelector('.hourglass-3-mask');
 
 function clockGlass() {
     setInterval(function clock() {
         hourglass4.style.transition = '60s linear';
-        hourglass4.style.height = '250px';
+        hourglass4.style.height = '249px';
         hourglass3.style.transition = '60s linear';
-        hourglass3.style.height = '250px';
+        hourglass3.style.height = '248px';
+        hourglassMask.style.transition = '30s linear';
+        hourglassMask.style.height = '105px';
     }, 1000);
-
+    // document.addEventListener('click', () => {
+    //     clockGlass()
+    // })
 }
 
 clockGlass()
